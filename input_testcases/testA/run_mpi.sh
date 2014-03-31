f68dbeb55773e385e587a8e546056ced462781be
Fahad Khalid <fahad.khalid@hpi.uni-potsdam.de>
2014-03-31 15:17:00 +0200

    Updated makefiles in Release with mpicc. Added new test files.

---
+++ input_testcases/testA/run_mpi.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+basefile="input_testcases/testA/input.txt"
+parafile="input_testcases/parameters.txt"
+temppara="input_testcases/testA/temp_par.txt"
+
+#run with vary_sm:
+ln1="OutputForecastFile=output_testcases/testA1"
+ln2="Logfile=output_testcases/testA1.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  > temp_inputA
+sed "41s+0+1+"  $parafile > $temppara
+
+mpirun -np 1 Release/CRS_3.0 temp_inputA
+#Release/CRS_3.0 temp_inputA
+
+#run with vary_fm:
+ln1="OutputForecastFile=output_testcases/testA2"
+ln2="Logfile=output_testcases/testA2.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  > temp_inputA
+sed "42s+0+1+"  $parafile > $temppara
+
+mpirun -np 1 Release/CRS_3.0 temp_inputA
+
+#run with vary_fm:
+ln1="OutputForecastFile=output_testcases/testA3"
+ln2="Logfile=output_testcases/testA3.log"
+ln11="InputListCatalogFocMecFile=input_testcases/listfocmecfiles.txt"
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  | sed "11s+.*+$ln11+" > temp_inputA
+sed "42s+0+1+"  $parafile > $temppara
+
+mpirun -np 1 Release/CRS_3.0 temp_inputA
+
+#run with vary_grid:
+ln1="OutputForecastFile=output_testcases/testA4"
+ln2="Logfile=output_testcases/testA4.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  > temp_inputA
+sed "43s+0+1+"  $parafile > $temppara
+
+mpirun -np 1 Release/CRS_3.0 temp_inputA
+rm temp_inputA
+rm $temppara
---
+++ input_testcases/testB/run_mpi.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+basefile="input_testcases/testB/input.txt"
+parafile="input_testcases/parameters_uncert.txt"
+temppara="input_testcases/testB/temp_par.txt"
+
+#run with catalog2 (mainshock at t=0 moved back by 100 days):
+ln1="OutputForecastFile=output_testcases/testB1"
+ln2="Logfile=output_testcases/testB1.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+" | sed "7s+catalog+catalog2+" > temp_inputB
+cp  $parafile $temppara
+
+mpirun -np 2 Release/CRS_3.0 temp_inputB
+
+#run with catalog2 (mainshock at t=0 removed):
+ln1="OutputForecastFile=output_testcases/testB2"
+ln2="Logfile=output_testcases/testB2.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+" | sed "7s+catalog+catalog3+" > temp_inputB
+cp $parafile $temppara
+
+mpirun -np 2 Release/CRS_3.0 temp_inputB
+
+rm temp_inputB
+rm $temppara
