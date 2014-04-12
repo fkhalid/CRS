c04c667d801761ba5b14bfa8a309f0f71ab3000c
Fahad Khalid <fahad.khalid@hpi.uni-potsdam.de>
2014-04-12 14:38:00 +0200

    Added run_mpi.sh files.

---
+++ input_testcases/testD/run_mpi.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+basefile="input_testcases/testD/input.txt"
+parafile="input_testcases/parameters_aslip.txt"
+temppara="input_testcases/testD/temp_par.txt"
+
+#run with vary_sm:
+ln1="OutputForecastFile=output_testcases/testD1"
+ln2="Logfile=output_testcases/testD1.log"
+
+#afterslip model with a single snapshot:
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  > temp_inputD
+echo "InputListAfterslipModels=input_testcases/testD/aslipmodels1.dat" >> temp_inputD
+sed "41s+0+1+"  $parafile > $temppara
+mpirun -np 2 Release/CRS_3.0 temp_inputD
+
+#afterslip model with multiple snapshots (splines):
+ln1="OutputForecastFile=output_testcases/testD2"
+ln2="Logfile=output_testcases/testD2.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  > temp_inputD
+echo "InputListAfterslipModels=input_testcases/testD/aslipmodels2.dat" >> temp_inputD
+sed "42s+0+1+"  $parafile > $temppara
+
+mpirun -np 2 Release/CRS_3.0 temp_inputD
+
+rm temp_inputD
+rm $temppara
---
+++ input_testcases/testE/run_mpi.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+basefile="input_testcases/testE/input.txt"
+parafile="input_testcases/parameters_uncert.txt"
+temppara="input_testcases/testE/temp_par.txt"
+
+#run with internal background rate calculations (but not enough events available):
+#ln1="OutputForecastFile=output_testcases/testE1"
+#ln2="Logfile=output_testcases/testE1.log"
+
+#sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+"  > temp_inputE
+#sed "50s+0+1+"  $parafile > $temppara
+#Release/CRS_3.0 temp_inputE
+
+#run with internal background rate calculations (not enough events available after declustering):
+#ln1="OutputForecastFile=output_testcases/testE2"
+#ln2="Logfile=output_testcases/testE2.log"
+
+#sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+" | sed "7s+catalog+catalog4+" > temp_inputE
+#sed "50s+0+1+"  $parafile > $temppara
+
+#Release/CRS_3.0 temp_inputE
+
+#run with internal background rate calculations (enough events available):
+ln1="OutputForecastFile=output_testcases/testE3"
+ln2="Logfile=output_testcases/testE3.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+" | sed "7s+catalog+catalog5b+" > temp_inputE
+sed "50s+0+1+"  $parafile > $temppara
+mpirun -np 2 Release/CRS_3.0 temp_inputE
+
+
+#run with externally calculated background rate (same as previous case):
+ln1="OutputForecastFile=output_testcases/testE4"
+ln2="Logfile=output_testcases/testE4.log"
+
+sed "1s+.*+$ln1+" $basefile | sed "2s+.*+$ln2+" | sed "7s+catalog+catalog5b+" > temp_inputE
+echo "InputBackgroundRateFile=input_testcases/background3.dat" >> temp_inputE
+
+mpirun -np 2 Release/CRS_3.0 temp_inputE
+
+rm temp_inputE
+rm $temppara
