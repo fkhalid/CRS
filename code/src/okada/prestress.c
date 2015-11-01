fkhalid/CRS

commit 3b0d24802cd598ed733c00182cd30f5306fb49e4

Went through todos.

code/src/okada/prestress.c
@@ -52,7 +52,7 @@ void prestress(double s1, double s2, double s3, double strike, double dip, doubl
       double  ns[3],ts[3],rst[3],rdi[3];
       double  sig[3],rot[3][3];
 
-      *s=d2array(1,3,1,3);	//TODO deallocate at the end
+      *s=d2array(1,3,1,3);
       if(s1==0.0 && s2==0.0 && s3==0.0) return;
 
       cmb1=0.5*fabs(s2-s3)*sqrt(1+f*f)+f*(0.5*(s2+s3)+p);
@@ -119,7 +119,7 @@ void prestress(double s1, double s2, double s3, double strike, double dip, doubl
       }
       for (int j=0; j<3; j++) for (int i=2; i>j; i--)(*s)[i+1][j+1]=(*s)[j+1][i+1];
 
-      
+     free_d2array(*s,1,3,1,3);
      return;
 }
 

code/main.c
@@ -599,7 +599,6 @@ int main (int argc, char **argv) {
 
 	// if (LLinversion && forecast &&  tendLL<=Tstart) the results from LLinversion will be saved and used for forecast calculation.
 	// gammas_new is overwritten for each (Asig, ta) value; the values for the optimal value are saved in gammas_maxLL.
-	// TODO: these variables (gammas_new and gammas_maxLL may be too large to be stored in memory (for large Nsur). 
 
 	print_screen("Setting up variables needed for grid search...");
 
@@ -1068,7 +1067,5 @@ int main (int argc, char **argv) {
 	#endif
 
 	return 0;
-
-	//todo free memory.
 
 }

code/src/general/forecast_stepG.c
@@ -107,7 +107,7 @@ int rate_state_evolution(struct catalog cat, double *times, double **cmpdata, st
 		firsttimein=0;
 
 		//allocate memory:
-		NeXdum=darray(1,N);	//todo this and the one below are never deallocated.
+		NeXdum=darray(1,N);
 		ReX=darray(1,N);
 
 		//----------------------------------------------------------------------------------------------//

code/src/general/setup.c
@@ -212,7 +212,7 @@ int setup_aseismic_element(struct eqkfm *eqkfm0res, char **slipmodels, char *cmb
 	 *  eqkfm0res is populated. size [0...Nfaults] (Nfaults is read from the slip model below).
 	 */
 
-	//TODO may also want to allow individual snapshots of allslip_xxx to be NULL (e.g. eqkfm0res[f].allslip_xxx[t]=NULL if there is no slip at time t).
+	//todo to save memory, may also want to allow individual snapshots of allslip_xxx to be NULL (e.g. eqkfm0res[f].allslip_xxx[t]=NULL if there is no slip at time t).
 	int err=0, NF;
 	double 	toll=1e-10;
 	struct eqkfm *eqkfm0;	//used to read individual slip models (one per snapshot), later copied into eqkfm0res structure.
@@ -477,7 +477,7 @@ int setup_CoeffsDCFS(struct Coeff_LinkList **Coefficients, struct Coeff_LinkList
 
     if (Coefficients!=NULL) {
     	//Create elements of structure (allocate memory):
-    	AllCoeff= malloc( sizeof(struct Coeff_LinkList));	//TODO deallocate at the end.
+    	AllCoeff= malloc( sizeof(struct Coeff_LinkList));
 		temp= AllCoeff;
 		for(int i=0; i<Nm; i++) {
 			temp->borrow_coeff_from=NULL;	// coefficients must be be calculated for all seismic sources.

code/src/general/setup_time.c
@@ -498,9 +498,6 @@ int setup_aseismic_single_log(double t0, double t1, double ts,
 
 	err=timesteps_omori(t0, t1, eqk_aft, NA, Nfaults, L, times2, smallstepstime);
 
-//todo delete
-	print_screen("L=%d\n",*L);
-
 	// Temporal evolution of aseismic slip.//
 	nfaults=0;
 	for (int nev=0; nev<NA; nev++){

code/src/general/struct_conversions.c
@@ -592,7 +592,7 @@ void eqkfm2dist(struct eqkfm *eqkfm1, double *lats, double *lons, double *depths
 
 			latlon2localcartesian(eqkfm1[i].lat, eqkfm1[i].lon, lat0, lon0, &y, &x);
 			Depth=eqkfm1[i].depth;
-			eqkfm1[i].distance=darray(1,nsel);	//TODO deallocate.
+			eqkfm1[i].distance=darray(1,nsel);
 
 			for (int p=1; p<=nsel; p++) {
 				pt=eqkfm1[i].selpoints[p];

code/src/inp_out/read_csep_template.c
@@ -241,31 +241,7 @@ int read_csep_template(char *fname, int *no_magbins, int *nlat, int *nlon,
 			}
 		}
 
-//		if (fabs(data[3][n1]-data[3][1])>toll) closest_lat=fmin(closest_lat,fabs(data[3][n1]-data[3][1]));
-//		if (fabs(data[1][n1]-data[1][1])>toll) closest_lon=fmin(closest_lon,fabs(data[1][n1]-data[1][1]));
-//		if (fabs(data[5][n1]-data[5][1])>toll) closest_dep=fmin(closest_dep,fabs(data[5][n1]-data[5][1]));
 	}
-
-//	if ((dep1-dep0-ddepi)<toll) closest_dep=dep1-dep0;	//there is only one depth layer;
-//	if ((lat1-lat0-dlati)<toll) closest_lat=lat1-lat0;	//there is only one lat layer;
-//	if ((lon1-lon0-dloni)<toll) closest_lon=lon1-lon0;	//there is only one lon layer;
-//
-//	//calculated for uniform grid: (toll since casting is same as floor).
-//	//fixme testing for uniform grid gives false positives!
-//	if (fabs(closest_lat-dlati)<toll && fabs(closest_lon-dloni)<toll && fabs(closest_dep-ddepi)<toll) {
-//
-//		if (uni)  *uni=1;
-//		if (nlat) *nlat= (int) (toll+(lat1-lat0)/dlati);
-//		if (nlon) *nlon= (int) (toll+(lon1-lon0)/dloni);
-//		if (ndep) *ndep= (int) (toll+(dep1-dep0)/ddepi);
-//	}
-//
-//	else {
-//		if (uni)  *uni=0;
-//		if (nlat) *nlat= 0;
-//		if (nlon) *nlon= 0;
-//		if (ndep) *ndep= 0;
-//	}
 
         if (nlat) *nlat= (int) (toll+(lat1-lat0)/dlati);
         if (nlon) *nlon= (int) (toll+(lon1-lon0)/dloni);

commit 3ed83cc32e7500ea2eb58741ad42b88500c20ee5

Fixed memory bug introduced in 3b0d24802cd598ed733c00182cd30f5306fb49e4.wq

code/src/okada/prestress.c
@@ -119,7 +119,6 @@ void prestress(double s1, double s2, double s3, double strike, double dip, doubl
       }
       for (int j=0; j<3; j++) for (int i=2; i>j; i--)(*s)[i+1][j+1]=(*s)[j+1][i+1];
 
-     free_d2array(*s,1,3,1,3);
      return;
 }
 